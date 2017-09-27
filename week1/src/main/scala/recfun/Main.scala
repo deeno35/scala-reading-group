597332c5d643fa21f62ab1f3fbf8b2b48d4c868b
David Nguyen <davidn@uber.com>
2017-09-26 23:48:28 -0700

    Add Week1 answers

--- week1/src/main/scala/recfun/Main.scala
+++ week1/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
 import common._
+import org.scalacheck.Prop.True
 
 object Main {
   def main(args: Array[String]) {
@@ -14,15 +15,53 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r){
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def recursiveBalance(chars: List[Char], parenCount: Int): Boolean = {
+      if(parenCount < 0) {
+        // At any point if we have too many close parens, short circuit out
+        false
+      } else if(chars.isEmpty) {
+        // We are on the last character.  If 0, we have balanced paren counts
+        // otherwise we have too many open parens
+        if(parenCount == 0) {
+          true
+        } else {
+          false
+        }
+      } else {
+        chars.head.toString match {
+          case "(" => recursiveBalance(chars.tail, parenCount + 1)
+          case ")" => recursiveBalance(chars.tail, parenCount - 1)
+          case _ => recursiveBalance(chars.tail, parenCount)
+        }
+      }
+    }
+
+    recursiveBalance(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) {
+      1
+    } else if (money > 0 && coins.nonEmpty) {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    } else {
+      0
+    }
+
+  }
 }
